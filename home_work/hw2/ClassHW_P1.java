ecd31270ce2a83efbf95fc81d76ab34bfa27ad1e
Jon <18120090+jonhogan@users.noreply.github.com>
2021-09-22 20:36:10 -0500

    Start HW2

---
+++ home_work/hw2/ClassHW_P1.java
@@ -0,0 +1,21 @@
+/**
+*   Author      Jonathan Hogan
+*   Class       Dr.Das - CMPS 4143 Contemporary Programming Languages
+*   Due         10/11/21                                                   
+*   
+*    Program for Question 1: Write an OOP code (A case study:
+*    Should be unique; If you are copying code from online you will
+*    be caught) that has the following OOP features: inheritance(any),
+*    polymorphism (runtime and compile time), abstraction and
+*    encapsulation (20 points)
+*/
+
+import java.util.*;
+
+public class ClassHW_P1
+{
+    public static void main(String[] args)
+    {
+
+    }
+} 
