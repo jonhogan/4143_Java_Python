2262746adeda024b5292b4a99602bf5e6469a5cb
Jon <18120090+jonhogan@users.noreply.github.com>
2021-10-01 16:17:35 -0500

    Review

---
+++ study/test.java
@@ -0,0 +1,23 @@
+public class test {
+    public static void main(String[] args)
+    {
+
+        String fdsa = "";
+
+        double fN = 1.67d;
+        float asdf = 1.67f;
+
+        System.out.println("I'm so busy");
+
+        int intArray[] = {1,2,3,4,5,6,7,8,9};
+
+        int bArray[];
+
+        bArray = intArray;
+
+        for (int i = 0; i < bArray.length; i++)
+        {
+            System.out.println(bArray[i]);
+        }
+    }
+}
