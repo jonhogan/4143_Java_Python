991277dd6a102c95ce1436d41f2831ab12348b5d
Jon Hogan <jon.hogan83@gmail.com>
2021-10-09 00:18:24 -0500

    Update HW 2

--- home_work/hw2/q1/ClassHW_P1.java
+++ home_work/hw2/q1/ClassHW_P1.java
@@ -16,21 +16,36 @@ public class ClassHW_P1
     public static void main(String[] args)
     {
         String name;
+        String weapon;
         Scanner scan= new Scanner(System.in);
-        playerChar p1;
+        PlayerChar p1;
         
         System.out.println("Name your character");
-        System.out.println("Entering a blank line will give you a default name.");
+        System.out.println("Entering a blank line will give you a default name.\n");
         name = scan.nextLine();
-        if (name.equals("")){
-            p1 = new playerChar();
-        }else{
-            p1 = new playerChar(name);
-        }
-     
-        //System.out.println(p1.getStr());
 
-        System.out.println("Welcome, " + p1.getName() +". On a table before you there is equipment. Prepare for battle!");
+        if (name.equals("")){
+            p1 = new PlayerChar();
+        }else{
+            p1 = new PlayerChar(name);
+        }
+
+        Gnoll gnoll = new Gnoll();
+     
+        p1.printStats();
+
+        System.out.println("Welcome, " + p1.getName() +".\n\nYou open your eyes, heavy with sleep. The sound of twigs snapping"
+        + " awakend you. Prepare for battle!\nChoose your weapon: Mace, Dagger, Staff");
+        
+        weapon = scan.nextLine();
+
+        System.out.println("\nYou ready your " + weapon.toLowerCase() + ". Suddenly a gnoll appears and rushed you!");
+
+        while(p1.currHP < 0 & gnoll.currHP < 0){
+            p1.attack(weapon);
+            gnoll.attack();
+        }
+
 
 
 
---
+++ home_work/hw2/q1/Gnoll.java
@@ -0,0 +1,16 @@
+public class Gnoll extends Character{
+
+    Gnoll(){
+
+        level = 1;
+        maxHP = 22;
+        str = 14;
+        intel = 6;
+        dex = 12;
+        cons = 11;
+        wis = 10;
+        charisma = 7;
+        currHP = maxHP;
+        name = "Gnoll";
+    }
+}
