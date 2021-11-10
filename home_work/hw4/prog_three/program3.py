0e1401e63c9ea7a707d5e97b787aab1e4834e1f2
Jon <18120090+jonhogan@users.noreply.github.com>
2021-11-09 19:30:49 -0600

    Updated HW4.3

--- home_work/hw4/prog_three/program3.py
+++ home_work/hw4/prog_three/program3.py
@@ -10,3 +10,168 @@ account; either saving or checking or both. Implement the scenario using python 
 and make sure you have covered those OOP concepts on your code: inheritance(any),
 polymorphism (runtime and compile time), abstraction and encapsulation.
 '''
+
+
+class BankAccount:
+
+    def __init__(self, name, CheckingAccount, SavingsAccount):
+        self.name = name
+        self.age = age
+        self._balance = _balance
+
+    def get_info(self):
+        print("Name:\t{}".format(self.name))
+        print("Age: \t{}".format(self.age))
+        self.print_balance()
+
+    def get_balance(self):
+        return self._balance
+
+    def print_balance(self):
+        print("Current Balance:\t{0:.2f}".format(self.get_balance()))
+
+
+def deposit_money(self, amount):
+    if(type(amount) == int and amount > 0):
+        self._balance += amount
+        print("Deposit amount:\t{0:.2f} ".format(amount))
+
+    else:
+        print("Please enter valid data for amount")
+
+
+class SavingAccount(BankAccount):
+
+    def __init__(self, name, age, balance, interest_rate):
+        BankAccount.__init__(self, name, age, balance)
+        self._interest_rate = interest_rate
+
+    def get_info(self):
+        print("Account Type: Savings")
+        print("Name:\t{}".format(self.name))
+        print("Age: \t{}".format(self.age))
+        self.print_balance()
+        self.print_interest_rate()
+
+    def print_interest_rate(self):
+
+        print("Current rate of interest = {0:.2f}".format(self.get_interest_rate()))
+
+    def set_interest_rate(self, rate):
+
+        if(type(rate) == int and rate > 0):
+            self._interest_rate = rate
+
+        else:
+            print("Please enter valid data for interest rate")
+
+    def get_interest(self, years):
+        return self.get_interest_rate()*years*self.get_balance()/100
+
+    def get_interest_rate(self):
+        return self._interest_rate
+
+
+class CheckingAccount(BankAccount):
+
+    def __init__(self, name, age, balance, checks_issued):
+        BankAccount.__init__(self, name, age, balance)
+        self._checks_issued = checks_issued
+
+    def get_info(self):
+        print("Account Type: Current")
+        print("Name:\t{}".format(self.name))
+        print("Age: \t{}".format(self.age))
+        self.print_balance()
+        self.print_checks()
+
+    def print_checks(self):
+        print("Total number of checks issued: {}".format(self.get_checks_issued()))
+
+    def get_checks_issued(self):
+        return self._checks_issued
+
+    def issue_checks(self, n):
+        if(type(n) == int and n > 0):
+            self._checks_issued += n
+
+        else:
+            print("enter valid data for number of checks")
+
+
+class Customer(SavingAccount, CheckingAccount):
+
+    def __init__(self, name, age, balance, type):
+        self.type = type
+
+        if(type == "s"):
+            interest_rate = int(input("Enter interest rate: "))
+            SavingAccount.__init__(self, name, age, balance, interest_rate)
+
+        if(type == "c"):
+            checks_issued = int(input("Enter number of checks: "))
+            CheckingAccount.__init__(self, name, age, balance, checks_issued)
+
+        if(type == "s+c" or type == "c+s"):
+            rate = int(input("Enter interest rate: "))
+            checks_issued = int(input("Enter number of checks: "))
+            SavingAccount.__init__(self, name, age, balance, rate)
+            CheckingAccount.__init__(self, name, age, balance, checks_issued)
+
+    def get_info(self):
+
+        if(self.type == "s"):
+            SavingAccount.get_info(self)
+
+        elif(self.type == "c"):
+            CheckingAccount.get_info(self)
+
+        else:
+            print("Account Type: Savings + Checking")
+            print("Name:\t{}".format(self.name))
+            print("Age: \t{}".format(self.age))
+            self.print_balance()
+            self.print_interest_rate()
+            self.print_checks()
+
+
+Selection = ""
+while Selection != 8:
+
+    print("MAIN MENU")
+    print("1. NEW ACCOUNT")
+    print("2. DEPOSIT AMOUNT")
+    print("3. WITHDRAW AMOUNT")
+    print("4. BALANCE ENQUIRY")
+    print("5. ALL ACCOUNT HOLDER LIST")
+    print("6. CLOSE AN ACCOUNT")
+    print("7. MODIFY AN ACCOUNT")
+    print("8. EXIT")
+    print("Select Your Option (1-8) ")
+
+    Selection = input()
+    if Selection == '1':
+        BankAccount()
+
+    elif Selection == '2':
+        num = int(input("\tEnter The account No. : "))
+        depositAndWithdraw(num, 1)
+
+    elif Selection == '3':
+        num = int(input("\tEnter The account No. : "))
+        depositAndWithdraw(num, 2)
+
+    elif Selection == '4':
+        num = int(input("\tEnter The account No. : "))
+        displaySp(num)
+
+    elif Selection == '5':
+        displayAll()
+
+    elif Selection == '6':
+        print("\tThank you for banking with me")
+        break
+
+    else:
+        print("Invalid Selection")
+        Selection = input("Enter your Selection 1-8 : ")
