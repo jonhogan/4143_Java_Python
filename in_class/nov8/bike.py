4bb94c1935a727a32454fbb5cb31b4d3e871315d
Jon <18120090+jonhogan@users.noreply.github.com>
2021-11-09 19:31:04 -0600

    Update

--- in_class/nov8/bike.py
+++ in_class/nov8/bike.py
@@ -3,50 +3,44 @@ class Bicycle:
     gear = 0
     speed = 0
     
-    def init(self, g = 0, s = 0):
-        self.gear = g
-        self.speed = s
+    def __init__(self, _gear = 0, _speed = 0):
+        self.gear = _gear
+        self.speed = _speed
 
-    def brake(self, decrement=None):
-        self.speed-=decrement
+    def brake(self, decrement = 0):
+        self.speed -= decrement
 
-    def speedUp(self, increment=None):
-        self.speed+=increment
+    def speedUp(self, increment = 0):
+        self.speed += increment
 
-    def status(self):
-        print(f'Speed is {self.speed}\n Gear is {self.gear}')
+    def info(self):
+        print('Speed is {}\n Gear is {}'.format(self.speed, self.gear))
 
-# Mountain Bike inh. from Bicycle
+# Mountain Bike derived from Bicycle
 class mountainBike(Bicycle):
-    seatHeight=int()
+    seat = 0
 
-    def init(self, g = 0, s = 0, sh = 0):
-        super().init(g,s)
-        self.seatHeight = sh
+    def __init__(self, _gear, _speed, height = 0):
+        super().__init__(_gear, _speed)
+        self.seat = height
 
-    def status(self):
-        print(f'Speed is {self.speed}\n Gear is {self.gear} \n seat Height {self.seatHeight}')
+    def info(self):
+        print('Speed is {}\n Gear is {} \n Seat height {}'.format(self.speed, self.gear, self.seat))
 
-# Make an objects for Bike and Mountain Bike
-mb1 = mountainBike(2,26,3)
-bik1 = Bicycle(5,66)
+obj1 = mountainBike(3,10,2)
+obj2 = Bicycle(4,10)
 
-# Get the status before
-print('the Status before changing speed')
-print('Mountain status is: ')
-mb1.status()
-print('Bicycle Bike status is: ')
-bik1.status()
+print('Current information on the mountain bike: ')
+obj1.info()
+print('Current information of the bike is: ')
+obj2.info()
 
-# Speed Up my mountain bike by 4 mph
-mb1.speedUp(4)
+obj1.speedUp(2)
 
-# Brake up my car by 6 mph
-bik1.brake(6)
+obj2.brake(3)
 
-# Get the status after
-print('the Status after changed the speed')
-print('Mountain status is: ')
-mb1.status()
-print('Bicycle Bike status is: ')
-bik1.status()
+print('After updating the speed')
+print('Mountain bike information is: ')
+obj1.info()
+print('Bike information is: ')
+obj2.info()
